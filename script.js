2622c547219e3e2353108aff8befa98225cbbd3a
Omer Abdur Rehman <omerrehman000@gmail.com>
2026-06-16 16:09:40 +0500

    Portfolio UI upgrade: added new projects and enhancements

--- script.js
+++ script.js
@@ -7,18 +7,43 @@ cards.forEach(card=>{
 const top=card.getBoundingClientRect().top
 
 if(top<window.innerHeight-100){
-
 card.style.opacity='1'
 card.style.transform='translateY(0)'
 }
 
 })
-
 })
 
 cards.forEach(card=>{
-
 card.style.opacity='0'
 card.style.transform='translateY(40px)'
 card.style.transition='1s'
 })
+
+let i=0
+let text="Python Developer | AI Engineer | Embedded Systems"
+
+function type(){
+document.getElementById("typing").innerHTML=text.slice(0,i++)
+if(i<=text.length){
+setTimeout(type,100)
+}
+}
+type()
+
+function toggleTheme(){
+document.body.classList.toggle("light")
+}
+
+function toggleMenu(){
+document.getElementById("navMenu").classList.toggle("active")
+}
+
+function openPopup(img){
+document.getElementById("popup").style.display="flex"
+document.getElementById("popup-img").src=img
+}
+
+function closePopup(){
+document.getElementById("popup").style.display="none"
+}
